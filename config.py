49e41b0437727cc32655c98da0b5b57c69243202
hoarf <alan.ficagna@gmail.com>
2015-12-11 18:05:11 -0500

    Simplified the config file and add support for sioux falls  - Removed the function runConfig because it was not necessary  - Passing the capacity file as argument correctly now. This should    make the sioux falls network correctly calculate the edges costs    with its own function.

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ class ExperimentConfig:
                network=ORTUZAR_NETWORK, network_od=ORTUZAR_NETWORK_OD,
                printInterval=1):
 
-      self.network_capacity = None
+      self.network_capacity = SIOUXFALLS_CAPACITY
       self.printLinkCosts = printLinkCosts
       self.printDriversPerLink = printDriversPerLink
       self.printPairOD = printPairOD
@@ -78,45 +78,34 @@ class ExperimentConfig:
       self.ks = ks
       self.GA_QL_Interval = interval #intervalo para GA->QL
 
-  def runByType(self, experimentType, k, network_capacity, group_size,
-                generations, alpha, decay, crossover, mutation, elite,
-                population, interval):
+  def runByType(self, k, group_size, alpha, decay, crossover, mutation,
+                interval):
     """
     Call the apropriate script to run the experiment based on experiment type
     """
 
-    ex = Experiment(k, self.network, network_capacity, self.network_od, group_size,
+    ex = Experiment(k, self.network, self.network_capacity, self.network_od, group_size,
     printLinkCosts=self.printLinkCosts, printDriversPerLink=self.printDriversPerLink,
     printPairOD=self.printPairOD, printInterval=self.printInterval)
 
-    if(experimentType==2): #GA only
+    if(self.experimentType==2): #GA only
         print("Running GA Only")
         print(mutation)
-        ex.run_ga_ql(False,False,generations, population, crossover,
-                      mutation, elite, None, None,None)
-    elif(experimentType==3):#GA<-QL
+        ex.run_ga_ql(False,False, self.generations, self.population, crossover,
+                      mutation, self.elite_size, None, None,None)
+    elif(self.experimentType==3):#GA<-QL
         print("Running GA<-QL ")
-        ex.run_ga_ql(True,False,generations, population, crossover,
-                      mutation, elite, alpha, decay,None)
-    elif(experimentType==4):#GA<->QL
+        ex.run_ga_ql(True,False, self.generations, self.population, crossover,
+                      mutation, self.elite_size, alpha, decay,None)
+    elif(self.experimentType==4):#GA<->QL
         print("Running GA<->QL ")
-        ex.run_ga_ql(True,True,generations, population, crossover,
-                      mutation, elite, alpha, decay,interval)
+        ex.run_ga_ql(True,True, self.generations, self.population, crossover,
+                      mutation, self.elite_size, alpha, decay, interval)
 
     ##FOR QL only use this method:
-    elif(experimentType==1): # QL only
+    elif(self.experimentType==1): # QL only
         print("Running QL Only ")
-        ex.run_ql(generations,alpha, decay)
-
-  def runConfig(self, r, g, a, d, c, m ,k, i):
-    """
-    Run a particular configuration
-    """
-    echo("Running config..")
-    self.runByType(self.experimentType, k, self.network_capacity, g,
-                   self.generations, a, d, c, m, self.elite_size,
-                   self.population, i)
-    echo("Config ran")
+        ex.run_ql(self.generations, alpha, decay)
 
   def buildParameters(self):
     """
@@ -125,32 +114,26 @@ class ExperimentConfig:
     """
     echo("Building arg list..")
     args = []
-    for r in range(self.repetitions):
-      for g in self.group_sizes:
+    for g in self.group_sizes:
         for a in self.alphas:
-          for d in self.decays:
-            for c in self.crossovers:
-              for m in self.mutations:
-                for k in self.ks:
-                  for i in self.GA_QL_Interval:
-                    args.append([r,g,a,d,c,m,k,i])
+            for d in self.decays:
+                for c in self.crossovers:
+                    for m in self.mutations:
+                        for k in self.ks:
+                            for i in self.GA_QL_Interval:
+                                args.append([g,a,d,c,m,k,i])
     return args
 
   def runArg(self, a):
     """
-    a: list of arguments expected to be, in order:
-      0: repetition
-      1: group_size
-      2: alpha
-      3: decay
-      4: crossover
-      5: mutation
-      6: k
-      7: interval
+    a: list of arguments
     """
-    assert len(a) == 8
-    echo("Running an arg")
-    self.runConfig(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7])
+    assert len(a) == 7
+    group_size, alpha, decay, crossover, mutation, k, interval = a
+    echo("Running an arg %s" % a)
+    for _ in range(self.repetitions):
+      self.runByType(k, group_size, alpha, decay, crossover, mutation,
+                     interval)
     echo("Arg ran.")
 
 
