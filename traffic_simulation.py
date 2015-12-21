a55dc34bfd10eac4d4edd2609aaf60ff7755f0bb
hoarf <alan.ficagna@gmail.com>
2015-12-21 11:09:10 -0500

    Refactor options  Improves options and messages across the board for the command line API

--- traffic_simulation.py
+++ traffic_simulation.py
@@ -7,74 +7,73 @@ p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatt
                             drivers going from different points in the ortuzar
                             and siouxfalls networks""")
 
-p.add_argument("-c", "--printLinkCosts", action="store_true", default=False,
-               help="Print link's costs at each iteration in the output file")
+p.add_argument("--printTravelTime", action="store_true", default=False,
+               help="Print link's travel time at each iteration in the output file")
 
 p.add_argument("-d", "--printDriversPerLink", action="store_true", default=False,
-               help="Print the number of drivers in at each iteration in the output file")
+               help="Print the number of drivers in each link in the output file")
 
-p.add_argument("-p", "--printPairOD", action="store_true", default=False,
+p.add_argument("-o", "--printPairOD", action="store_true", default=False,
                help="Print the average travel time for in the header in the output file")
 
 p.add_argument("-i", "--printInterval", type=int, default=1,
                help="Interval by which the messages are written in the output file")
 
-p.add_argument("--generations", type=int, default=400,
+p.add_argument("-g", "--generations", type=int, default=400,
                help="Maximum mumber of generations in each configuration")
 
-p.add_argument("--population", type=int, default=100,
+p.add_argument("-p", "--population", type=int, default=100,
                help="Size of population for the genetic algorithm")
 
-p.add_argument("--group_sizes", nargs="+", type=int, default=[100],
+p.add_argument("--grouping", nargs="+", type=int, default=[100],
                help="List of group sizes for drivers in each configuration")
 
-p.add_argument("--alphas", nargs="+", type=float, default=[.9],
-               help="List of learning in each configuration")
+p.add_argument("-a", "--alphas", nargs="+", type=float, default=[.9],
+               help="List of learning rates in each configuration")
 
 p.add_argument("--decays", nargs="+", type=float, default=[.99],
                help="List of decays in each configuration")
 
-p.add_argument("--crossovers", nargs="+", type=float, default=[0.2],
+p.add_argument("-c", "--crossovers", nargs="+", type=float, default=[0.2],
                help="List of rate of crossover in the population in each configuration")
 
-p.add_argument("--mutations", nargs="+", type=float, default=[0.001],
+p.add_argument("-m", "--mutations", nargs="+", type=float, default=[0.001],
                help="List of rate of mutations in each configuration")
 
 p.add_argument("--ks", nargs="+", type=int, default=[8],
-               help="<TODO WRITE HELP MESSAGE FOR THIS OPTION>")
+               help="List of the 'K' hyperparameters for the KSP (K-ShortestPath) Algorithm")
 
 p.add_argument("--intervals", nargs="+", type=int, default=[None],
-               help="<TODO WRITE HELP MESSAGE FOR THIS OPTION>")
+               help="List of intervals that signal the frequency the QL values are supposed to be fed into GA")
 
 p.add_argument("--repetitions", type=int, default=1,
                help="How many times it should be repeated")
 
-p.add_argument("--network", type=unicode, choices=['ortuzar', 'siouxfalls'], default='ortuzar',
+p.add_argument("--net", type=unicode, choices=['OW10_1', 'SF'], default='OW10_1',
                help="Which network should be used")
 
 p.add_argument("--experimentType", type=int, choices=[1,2,3,4], default=1,
-               help="""How many repetition for each configuration.
-               1 - Use Q-Learn Only
-               2 - Use Genetic Algorithm
-               3 - The Genetic Algorithm receives information from the Q-learn algorithm
-               4 - The Genetic Algorithm receives and updates information
-                   from the Q-learn algorithm
+               help="""
+               1 - QL only
+               2 - GA only
+               3 - QL builds solution for GA
+               4 - GA and QL exchange solutions
                """)
 
-p.add_argument("--elite_size", type=int, default=5,
+p.add_argument("-e", "--elite_size", type=int, default=5,
                help="How many elite individuals should be kept after each generation")
 
-p.add_argument("--number-of-processes", type=int, default=4,
+p.add_argument("--number-of-processes", type=int, default=1,
                help="How many parallel processes should be used to run the experiment configurations")
 
 a = p.parse_args()
 
 networks = {
-  'ortuzar': (config.ORTUZAR_NETWORK, config.ORTUZAR_NETWORK_OD),
-  'siouxfalls': (config.SIOUXFALLS_NETWORK, config.SIOUXFALLS_NETWORK_OD)
+  'OW10_1': (config.ORTUZAR_NETWORK, config.ORTUZAR_NETWORK_OD),
+  'SF': (config.SIOUXFALLS_NETWORK, config.SIOUXFALLS_NETWORK_OD)
 }
 
-configuration = cfg(printLinkCosts=a.printLinkCosts, printDriversPerLink=a.printDriversPerLink,
+configuration = cfg(printTravelTime=a.printTravelTime, printDriversPerLink=a.printDriversPerLink,
              printPairOD=a.printPairOD, generations=a.generations,
              population=a.population, repetitions=a.repetitions,
              experimentType=a.experimentType, elite_size=a.elite_size,
