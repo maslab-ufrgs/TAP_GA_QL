3aee1e5c9a458d47f9bd2e797d1fbc1a7f0c9e6a
Arthur Zachow <arthur.coelho@inf.ufrgs.br>
2016-11-20 17:16:36 -0200

    Routes problem

--- modules/ksp/function.py
+++ modules/ksp/function.py
@@ -31,3 +31,104 @@ def KShortestPaths(V, E, origin, destination, K):
  			break
 
 	return A
+
+def runKShortestPathsStep(V, E, origin, destination, k, A, B):
+	# Step 0: iteration 1
+	if k == 1:
+		A.append(findShortestPath(V, E, origin, destination, []))
+		
+	# Step I: iterations 2 to K
+	else:
+		lastPath = A[-1] 
+		for i in range(0, len(lastPath)-1):
+			# Step I(a)
+			spurNode = lastPath[i]
+			rootPath = lastPath[0:i+1]
+			toIgnore = []
+			
+			for path in A:
+				if path[0:i+1] == rootPath:
+					ed = getEdge(E, spurNode.name, path[i+1].name)
+					toIgnore.append(ed)
+			
+			# ignore the edges passing through nodes already in rootPath (except for the spurNode)
+			for noder in rootPath[:-1]: 
+				edgesn = pickEdgesListAll(noder, E)
+				for ee in edgesn:
+					toIgnore.append(ee)
+			
+			# Step I(b)
+			spurPath = findShortestPath(V, E, spurNode.name, destination, toIgnore)
+			if spurPath[0] != spurNode:
+				continue
+			
+			# Step I(c)
+			totalPath = rootPath + spurPath[1:]
+			B.append(totalPath)
+		
+		# handle the case where no spurs (new paths) are available
+		if not B:
+			return False
+			
+		# Step II
+		bestInB = None
+		bestInBlength = 999999999
+		for path in B:
+			length = calcPathLength(path, E)
+			if length < bestInBlength:
+				bestInBlength = length
+				bestInB = path
+		A.append(bestInB)
+		while bestInB in B:
+			B.remove(bestInB)
+		
+	return True
+	
+# Dijkstra's shortest path algorithm
+def findShortestPath(N, E, origin, destination, ignoredEdges):
+	
+	# reset the graph (so as to discard information from previous runs)
+	resetGraph(N, E)
+	
+	# set origin node distance to zero, and get destination node
+	dest = None
+	for node in N:
+		if node.name == origin:
+			node.dist = 0
+		if node.name == destination:
+			dest = node
+	
+	u = pickSmallestNode(N)
+	while u != None:
+		u.flag = 1
+		uv = pickEdgesList(u, E)
+		n = None
+		for edge in uv:
+			
+			# avoid ignored edges
+			if edge in ignoredEdges:
+				continue
+			
+			# take the node n
+			for node in N:
+				if node.name == edge.end:
+					n = node
+					break
+			if n.dist > u.dist + edge.length:
+				n.dist = u.dist + edge.length
+				n.prev = u
+		
+		u = pickSmallestNode(N)
+		# stop when destination is reached
+		if u == dest:
+			break
+	
+	# generate the final path
+	S = []
+	u = dest
+	while u.prev != None:
+		S.insert(0,u)
+		u = u.prev
+	S.insert(0,u)
+	
+	return S
