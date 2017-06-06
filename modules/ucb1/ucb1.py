25f55ad6e4b82831e65cee304dc6e4c1265e6137
Thiago Bell <thibef@gmail.com>
2017-06-06 00:55:29 -0300

    fixed problems with list initilization causing aliasing: lists pointing to same data structure

--- modules/ucb1/ucb1.py
+++ modules/ucb1/ucb1.py
@@ -8,23 +8,40 @@ import operator
 
 import random
 class UCB1():
-    def __init__(self, experiment, drivers, k):
+    def __init__(self, experiment, drivers, k,init_order):
         self.experiment = experiment
         self.k = k
+        self.init_order = init_order
         self.ODtable = {}
         self.drivers = drivers
         self.numdrivers=len(drivers)
-        self.number_plays = [[0]*k]*self.numdrivers
-        self.means = [[0.0]*k]*self.numdrivers
+        self.number_plays = []
+        self.means = []
+        for d in range(self.numdrivers):
+            self.number_plays.append([0]*k)
+            self.means.append([0.0]*k)
+
         self.round = [0]*self.numdrivers
 
     ##returns the route id the driver choosed
     def __choseActionDriver(self, dInx):
         self.round[dInx] += 1
         if (self.round[dInx] <= self.k):
-            ###plays one 'arm' once at the beginning
-            self.number_plays[dInx][self.round[dInx]-1] += 1
-            return self.round[dInx]-1
+            if(self.init_order == 1): ##random order init
+                possible_actions = []
+                for k in range(self.k):
+                    if self.number_plays[dInx][k] == 0:
+                        possible_actions.append(k)
+
+                choice = random.choice(possible_actions)
+                self.number_plays[dInx][choice] += 1
+                return choice
+            elif(self.init_order == 2): ##sequential order
+                self.number_plays[dInx][self.round[dInx] - 1] += 1
+                return self.round[dInx] - 1
+            else:
+                raise "invalid init order"
+
         else:  # regular case
             choice_value = [0.0] * self.k
             for i, u in enumerate(self.means[dInx]):
@@ -53,8 +70,8 @@ class UCB1():
 
         #updates the means. reward is the negative of the travel time
         for drIndex in range(self.numdrivers):
-#            reward = -traveltimes[drIndex]
-            reward = 1.0/traveltimes[drIndex]
+            reward = -traveltimes[drIndex]
+            #reward = 1.0/traveltimes[drIndex]
             self.__set_reward(drIndex, actions[drIndex], reward)
 
         average_tt_time = sum(traveltimes)/self.numdrivers
